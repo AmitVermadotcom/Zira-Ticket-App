9d8761f18304b1dc999445c9096db79132c779ce
Amit Verma <amitverma1050@gmail.com>
2021-04-21 21:13:03 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let TC = document.querySelector(".ticket-container");
-let filter = document.querySelectorAll(".filter");
-let add = document.querySelector(".add");
-for(let i=0;i<filter.length;i++){
-    filter[i].addEventListener("click",filterHandler);
-}
-
 // function filterHandler(e){
 //     console.log(e.currentTarget.children[0].classList[0].split("-")[0]);
 //     let clr = e.currentTarget.children[0].classList[0].split("-")[0];
@@ -18,8 +11,53 @@ function filterHandler(e){
     // console.log(styles);
     TC.style.backgroundColor = styles.backgroundColor;
 }
-add.addEventListener("click",popUp);
 
-function popUp(e){
-    add.innerHTML=``
+let TC = document.querySelector(".ticket-container");
+let allFilters = document.querySelectorAll(".filter");
+let modalVisible = false;
+
+for(let i = 0 ; i < allFilters.length; i++) {
+    allFilters[i].addEventListener("click", filterHandler);
+}
+
+function filterHandler (e) {
+
+}
+let addBtn = document.querySelector(".add");
+
+addBtn.addEventListener("click", showModal);
+
+function showModal(e) {
+    if(!modalVisible) {
+        // let modal = document.createElement("div");
+        // modal.classList.add("modal");
+        // modal.innerHTML = `<div class="task-to-be-added" data-typed="false" contenteditable="true"></div>
+        // <div class="modal-priority-list">
+        //     <div class="modal-pink-filter modal-filter"></div>
+        //     <div class="modal-blue-filter modal-filter"></div>
+        //     <div class="modal-green-filter  modal-filter"></div>
+        //     <div class="modal-yellow-filter  modal-filter"></div>
+        // </div>`;
+        // TC.appendChild(modal);
+
+        let modal = `<div class="modal">
+            <div class="task-to-be-added" data-typed="false" contenteditable="true">Enter your task here</div>
+            <div class="modal-priority-list">
+                <div class="modal-pink-filter modal-filter active"></div>
+                <div class="modal-blue-filter modal-filter"></div>
+                <div class="modal-green-filter  modal-filter"></div>
+                <div class="modal-yellow-filter  modal-filter"></div>
+            </div>
+        </div>`;
+        TC.innerHTML = TC.innerHTML + modal;
+        let taskModal = document.querySelector(".task-to-be-added");
+        taskModal.addEventListener("click", function(e) {
+            if(e.currentTarget.getAttribute("data-typed") == "false") {
+                e.currentTarget.innerText = "";
+                e.currentTarget.setAttribute("data-typed", "true");
+            }
+        })
+        modalVisible = true;
+    }
+
 }
